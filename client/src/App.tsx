e2327139458eae63014b8c6db0290e6f868242f0
adi061001 <adityasharma@thoughtwin.com>
2024-12-13 13:02:17 +0530

    just removed check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import Navbar from "../src/Layout/Navbar";
 import Footer from "./Layout/Footer";
 
 export default function App() {
-  const check = 'check'
   return (<>
     <Navbar />
     <Footer />
