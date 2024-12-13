2ae6b6a33e7dfb3426467ba3bccff92b60225bb5
adi061001 <adityasharma@thoughtwin.com>
2024-12-13 11:43:51 +0530

    testing changes

--- client/src/Dto/dataType.ts
+++ client/src/Dto/dataType.ts
@@ -1,22 +1,30 @@
-export interface loginDetails{
-    loginEmail:string|undefined
-    loginPassword:string
+export interface loginDetails {
+  loginEmail: string | undefined
+  loginPassword: string
 }
-export interface signUpDetails{
-    userName:String
-    userEmail:String
-    userPhone:String
-    userPassword:String
+export interface signUpDetails {
+  userName: String
+  userEmail: String
+  userPhone: String
+  userPassword: String
 }
 
 export interface buttonCs {
-    width: string
-    background: string
-    "border-radius": string,
-    "margin-top"?:string,
-    color?:string,
-    border?:string
-    "margin-bottom"?:string
+  width: string
+  background: string
+  "border-radius": string
+  "margin-top"?: string
+  color?: string
+  border?: string
+  "margin-bottom"?: string
+}
 
-  }
-  
+export interface buttonCsp {
+  width: number
+  background: string
+  "border-radius": string
+  "margin-top"?: string
+  color?: string
+  border?: string
+  "margin-bottom"?: string
+}
